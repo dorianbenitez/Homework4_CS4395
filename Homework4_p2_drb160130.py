45cad57f060cc4535f9dba2352cc13923c941423
Dorian Ray Benitez <48695978+dorianbenitez@users.noreply.github.com>
2020-09-22 04:53:17 -0500

    Update Homework4_p2_drb160130.py

--- Homework4_p2_drb160130.py
+++ Homework4_p2_drb160130.py
@@ -6,8 +6,26 @@
 #######
 
 import pickle
+from nltk import word_tokenize, ngrams
 
+
+# This function will calculate the probability of each language used per line
+def compute_prob(line, unigram, bigram, V):
+    p_laplace = 1
+    unigrams_tokens = word_tokenize(line)
+    bigrams_list = list(ngrams(unigrams_tokens, 2))
+
+    for b in bigrams_list:
+        n = bigram[b] if b in bigram else 0
+        d = unigram[b[0]] if b[0] in unigram else 0
+        p_laplace = p_laplace * ((n + 1) / (d + V))
+
+    return p_laplace
+
+
+# Main function
 if __name__ == '__main__':
+
     # Read the pickled dictionaries created in program 1
     english_unigram = pickle.load(open('english_unigram', 'rb'))
     english_bigram = pickle.load(open('english_bigram', 'rb'))
@@ -18,6 +36,9 @@ if __name__ == '__main__':
     italian_unigram = pickle.load(open('italian_unigram', 'rb'))
     italian_bigram = pickle.load(open('italian_bigram', 'rb'))
 
+    # Get the total length of all three unigrams
+    V = len(english_unigram) + len(french_unigram) + len(italian_unigram)
+
     # Open the test file and append each line to a list
     with open('LangId.test', 'r') as f:
         test_file = f.readlines()
@@ -25,7 +46,7 @@ if __name__ == '__main__':
 
     # Open the actual solution and append each each line to a list
     with open('LangId.sol', 'r') as f:
-        real_solution = f.readlines()
+        actual_solution = f.readlines()
     f.close()
 
     # Create/Write a new file to store our predicted solution
@@ -38,59 +59,39 @@ if __name__ == '__main__':
     correct_count = 0
     line_number = 0
 
-    # For each test file, calculate a probability for each language and write the
+    # For each test file, calculate the probability for each language and write the
     # language with the highest probability to a file.
-    # For each line in the test file, execute this loop
     for line in test_file:
         line_number += 1
-        english_count = 0
-        french_count = 0
-        italian_count = 0
 
-        # For each word in the line, analyze which language (English/French/Italian) is being used
-        for word in line.split():
-            if word in english_unigram:
-                english_count += 1
-            elif word in english_bigram:
-                english_count += 1
-            elif word in french_unigram:
-                french_count += 1
-            elif word in french_bigram:
-                french_count += 1
-            elif word in italian_unigram:
-                italian_count += 1
-            elif word in italian_bigram:
-                italian_count += 1
+        english_prob = compute_prob(line, english_unigram, english_bigram, V)
+        french_prob = compute_prob(line, french_unigram, french_bigram, V)
+        italian_prob = compute_prob(line, italian_unigram, italian_bigram, V)
 
-        # Write the language with the highest probability to a list and the "PredictedSolutionFile.txt" file
-        if english_count > french_count and english_count > italian_count:
-            predicted_solution.append(str(line_number) + " English\n")
-            f = open("PredictedSolutionFile.txt", "a")
-            f.write(str(line_number) + " English\n")
+        # Write the language with the highest probability to the "PredictedSolutionFile.txt" file
+        if english_prob > french_prob and english_prob > italian_prob:
+            with open('PredictedSolutionFile.txt', 'a') as f:
+                f.write(str(line_number) + " English\n")
             f.close()
 
-        elif french_count > english_count and french_count > italian_count:
-            predicted_solution.append(str(line_number) + " French\n")
-            f = open("PredictedSolutionFile.txt", "a")
-            f.write(str(line_number) + " French\n")
-            f.close()
-
-        elif italian_count > english_count and italian_count > french_count:
-            predicted_solution.append(str(line_number) + " Italian\n")
-            f = open("PredictedSolutionFile.txt", "a")
-            f.write(str(line_number) + " Italian\n")
+        elif french_prob > english_prob and french_prob > italian_prob:
+            with open('PredictedSolutionFile.txt', 'a') as f:
+                f.write(str(line_number) + " French\n")
             f.close()
 
         else:
-            predicted_solution.append(str(line_number) + " Unknown\n")
-            f = open("PredictedSolutionFile.txt", "a")
-            f.write(str(line_number) + " Unknown\n")
+            with open('PredictedSolutionFile.txt', 'a') as f:
+                f.write(str(line_number) + " Italian\n")
             f.close()
 
+    # Open the predicted solution file and append each line to a list
+    with open('PredictedSolutionFile.txt', 'r') as f:
+        predicted_solution = f.readlines()
+    f.close()
+
     # This compares the actual solution to our predicted solution
-    # and analyzes which lines were correctly and incorrectly predicted
     for i in range(len(predicted_solution)):
-        if predicted_solution[i] == real_solution[i]:
+        if predicted_solution[i] == actual_solution[i]:
             correct_count += 1
         else:
             wrong_lines.append(i + 1)
